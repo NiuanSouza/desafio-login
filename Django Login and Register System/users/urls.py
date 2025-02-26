b95b42a19c34e6e3175488da8ad435be92b1b893
NiuanSouza <162627331+NiuanSouza@users.noreply.github.com>
2025-02-26 14:41:42 -0300

    Add files via upload

---
+++ Django Login and Register System/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_view
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout_view, name="logout"),
+]
